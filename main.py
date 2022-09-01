42ac27611e4a83485030d06e3dfe2ed06877d9a2
ViniciusFantuchi <viniciusfantuchi@outlook.com>
2022-08-31 23:03:22 -0300

    Adicionado Frame header

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from PIL import Image, ImageTk
+import os
+
+PATH = os.path.dirname(os.path.realpath(__file__))
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
 
 
 class App(customtkinter.CTk):
@@ -18,19 +23,16 @@ class App(customtkinter.CTk):
         #self.protocol("WM_DELETE_WINDOW", self.on_closing())  # call .on_closing() when app gets closed
 
         #============ Set Frames =============
-
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=4)
         self.grid_columnconfigure(2, weight=4)
 
         self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=2)
-        self.grid_rowconfigure(2, weight=2)
-        #self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=6)
+        self.grid_rowconfigure(2, weight=6)
+        self.grid_rowconfigure(0, weight=1)
 
-        self.frame_header = customtkinter.CTkFrame(master=self,
-                                                    height=80,
-                                                    fg_color=None)
+        self.frame_header = customtkinter.CTkFrame(master=self, fg_color=None)
         self.frame_header.grid(row=0, column=0, columnspan=3, padx=20, pady=(20, 10), sticky="nswe")
 
         self.frame_objetives = customtkinter.CTkFrame(master=self)
@@ -47,12 +49,28 @@ class App(customtkinter.CTk):
 
 
         #============ Set Header =============
-
         self.obj_title = customtkinter.CTkLabel(self.frame_header, 
                                                 text="EXPLORER ROVER MISSION CONTROL",
                                                 text_font=("Roboto Bold", -20)
                                                 )
-        self.obj_title.grid(row=2, column=2)
+        self.obj_title.grid(row=0, column=0, sticky="sw")
+
+        self.obj_subtitle = customtkinter.CTkLabel(self.frame_header, 
+                                                text="KSCIA - International Space Academy",
+                                                text_font=("Roboto regular", -14),
+                                                justify=tkinter.LEFT
+                                                )
+        self.obj_subtitle.grid(row=1, column=0, sticky="nw")
+
+
+        logo = Image.open(PATH + "/images/logoKSCIA.png")
+        self.logo_image = ImageTk.PhotoImage(logo)
+
+        self.logo_label = customtkinter.CTkLabel(master=self.frame_header, image=self.logo_image)
+        self.logo_label.place(x=1130, y=0)
+
+    #============ Frame ============
+
 
 
     def on_closing(self, event=0):
